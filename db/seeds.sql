ea3857aa5dae92f88e07f1e23cc9b6858586484a
Valeria <valeria.szymanski.gomez@gmail.com>
2019-08-06 20:39:24 -0700

    okay

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quinoa Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
